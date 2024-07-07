d73b4bce52cb181ebd72117150e93ddc596aeacc
Mjturn <Mjturn@outlook.com>
2024-07-07 18:21:44 -0400

    initialize convertImageToASCII function

--- logic.ts
+++ logic.ts
@@ -1,7 +1,20 @@
-const uploadImageButton = document.querySelector("#upload-image-button") as HTMLInputElement
 const image = document.querySelector("#image") as HTMLImageElement
 
-uploadImageButton.addEventListener("change", () => {
-    image.src = URL.createObjectURL(uploadImageButton.files[0])
-    image.style.display = "block"
-})
+function displayImage() {
+    const uploadImageButton = document.querySelector("#upload-image-button") as HTMLInputElement
+
+    uploadImageButton.addEventListener("change", () => {
+        image.src = URL.createObjectURL(uploadImageButton.files[0])
+        image.style.display = "block"
+    })
+}
+
+function convertImageToASCII() {
+    const convertButton = document.querySelector("#convert-button")
+
+    convertButton.addEventListener("click", () => {
+    })
+}
+
+displayImage()
+convertImageToASCII()
